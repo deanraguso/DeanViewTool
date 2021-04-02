db7d56cd4296397317726a4f7d10192cb28d2308
dean <deanraguso@gmail.com>
2021-04-02 13:10:07 +1100

    Initial Commit

---
+++ lib/dean_view_tool.rb
@@ -0,0 +1,6 @@
+require "dean_view_tool/version"
+
+module DeanViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dean_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DeanViewTool
+  VERSION = "0.1.0"
+end
